bfa81b8e4d9a959ff97bd2a425f9674cc5c8c431
goblinious1 <112513403+goblinious1@users.noreply.github.com>
2025-01-29 12:10:07 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@
         3. PROFIT!!!
       </ClickableGlass>
       <ClickableGlass title="Cookie Corruption (main unblocking exploit)" expandable="true" width="90%">
-        1. Go to <a href="https://rebrand.ly/deadcurly" target="_blank" rel="noopener noreferrer">this page</a><br>
-        2. Paste the following text into the url bar of that page, remove the X and push enter:<br>
-        xjavascript:document.cookie='crextn_clear_cache_at=2024=01-21T05:59:59+00:00;'
-        <b>YOU CAN ALSO ADD THIS JAVASCRIPT AS A BOOKMARK AND USE IT ON THE "This site has been blocked" PAGE OF SECURLY. <br>
-          more info in the js thingies section</b>
+        1. create a bookmark for any page, and right click on it and select edit.<br>
+        2. paste the text below into the "url" box<br>
+        javascript:document.cookie='crextn_clear_cache_at=2024=01-21T05:59:59+00:00;'<br>
+        3. when you are on the securly blocked screen, click the bookmark
+        
       </ClickableGlass>
      
      
@@ -24,11 +24,11 @@
         to use these you gotta make a bookmark, edit it and paste the thing below as the url, then just click the bookmark to activate the thingy<br>
         <ul>
           <h3>website editor</h3>
-          <li><input type="text" readonly value="javascript:document.body.contentEditable = !javascript:document.body.contentEditable"></li>
+          <li><input type="text" readonly value="javascript:document.body.contentEditable = true;"></li>
           <h3>the hydraulic press</h3>
-          <li><input type="text" readonly value="javascript:(function(){var all=document.getElementsByTagName(\"*\");var depth=window.prompt(\"What depth should the crushing happen at?%20[leave%20empty%20for%20default]%20(how%20many%20nested%20elements%20does%20an%20element%20need%20to%20be%20considered%20ineligible%20for%20crushing)%22);function%20depthCheck(number,element){if(element.getAttribute(%22crushable%22)){return%20true;}if(element.children.length%3E0){if(number==0){return%20false;}for(var%20childIndex=0;childIndex%3Celement.children.length;childIndex++){var%20childCheck=depthCheck(number-1,element.children[childIndex]);if(childCheck==false){return%20false;}}}element.setAttribute(%22crushable%22,%22%22);return%20true;}var%20accel=0.1;var%20intervalTime=500;var%20accelRate=1.05;var%20minInterval=10;for(var%20i=0,max=all.length;i%3Cmax;i++){if(!depthCheck(depth,all[i])){console.log(%22Failed%20Depth%20Check...%22);continue;}console.log(%22Depth%20Check%20Succeeded:%22);var%20element=all[i];var%20f=element.getBoundingClientRect();element.style.top=f[%22y%22]+%22px%22;element.style.left=f[%22x%22]+%22px%22;element.style.width=f[%22width%22]+%22px%22;element.style.height=f[%22height%22]+%22px%22;element.style.position=%22absolute%20!important%22;(function%20crushElement(l,el){var%20crushInterval=setInterval(function(){var%20speed=accel*3\/400000+300;console.log(%22Crushing%20at%20speed:%22,speed);el.style.top=Math.max(el.style.top.slice(0,-2)-speed,0)+%22px%22;el.style.left=Math.max(el.style.left.slice(0,-2)-speed,0)+%22px%22;el.style.width=Math.max(el.style.width.slice(0,-2)-speed\/20,0)+%22px%22;el.style.height=Math.max(el.style.height.slice(0,-2)-speed\/20,0)+%22px%22;accel+=0.1;intervalTime=Math.max(intervalTime*accelRate,minInterval);if(el.style.width===%220px%22&&el.style.height===%220px%22){clearInterval(crushInterval);}},intervalTime);})(i,element);}})();"></li>
+          <li><input type="text" readonly value="javascript:(function(){var all=document.getElementsByTagName(&quot;*&quot;);var depth=window.prompt(&quot;What depth should the crushing happen at? [leave empty for default] (how many nested elements does an element need to be considered ineligible for crushing)&quot;);function depthCheck(number,element){if(element.getAttribute(&quot;crushable&quot;)){return true;}if(element.children.length&gt;0){if(number==0){return false;}for(var childIndex=0;childIndex&lt;element.children.length;childIndex++){var childCheck=depthCheck(number-1,element.children[childIndex]);if(childCheck==false){return false;}}}element.setAttribute(&quot;crushable&quot;,&quot;&quot;);return true;}var accel=0.1;var intervalTime=500;var accelRate=1.05;var minInterval=10;for(var i=0,max=all.length;i&lt;max;i++){if(!depthCheck(depth,all[i])){console.log(&quot;Failed Depth Check...&quot;);continue;}console.log(&quot;Depth Check Succeeded:&quot;);var element=all[i];var f=element.getBoundingClientRect();element.style.top=f[&quot;y&quot;]+&quot;px&quot;;element.style.left=f[&quot;x&quot;]+&quot;px&quot;;element.style.width=f[&quot;width&quot;]+&quot;px&quot;;element.style.height=f[&quot;height&quot;]+&quot;px&quot;;element.style.position=&quot;absolute !important&quot;;(function crushElement(l,el){var crushInterval=setInterval(function(){var speed=accel*3/400000+300;console.log(&quot;Crushing at speed:&quot;,speed);el.style.top=Math.max(el.style.top.slice(0,-2)-speed,0)+&quot;px&quot;;el.style.left=Math.max(el.style.left.slice(0,-2)-speed,0)+&quot;px&quot;;el.style.width=Math.max(el.style.width.slice(0,-2)-speed/20,0)+&quot;px&quot;;el.style.height=Math.max(el.style.height.slice(0,-2)-speed/20,0)+&quot;px&quot;;accel+=0.1;intervalTime=Math.max(intervalTime*accelRate,minInterval);if(el.style.width===&quot;0px&quot;&amp;&amp;el.style.height===&quot;0px&quot;){clearInterval(crushInterval);}},intervalTime);})(i,element);}})();"></li>
           <h3>rainbowifier</h3>
-          <li><input type="text" readonly value="javascript:(function(){var speed=prompt(\"Enter the animation speed in seconds (default is 5):\",\"5\");if(speed===null||speed.trim()===\"\"||isNaN(speed)){speed=5;}var css=`*:not(img):not(svg){background-image:linear-gradient(270deg,#FF0000,#FF7F00,#FFFF00,#00FF00,#0000FF,#4B0082,#8B00FF);animation:rainbow%20${speed}s%20infinite;background-size:400%%20400%;color:white!important;}@keyframes%20rainbow{0%{background-position:0%%2050%;}50%{background-position:100%%2050%;}100%{background-position:0%%2050%;}}%60;var%20style=document.createElement(\'style\');style.appendChild(document.createTextNode(css));document.head.appendChild(style);})();"></li>
+          <li><input type="text" readonly value="javascript:(function(){var speed=prompt(&quot;Enter the animation speed in seconds (default is 5):&quot;,&quot;5&quot;);if(speed===null||speed.trim()===&quot;&quot;||isNaN(speed)){speed=5;}var css=`*:not(img):not(svg){background-image:linear-gradient(270deg,#FF0000,#FF7F00,#FFFF00,#00FF00,#0000FF,#4B0082,#8B00FF);animation:rainbow%20${speed}s%20infinite;background-size:400%%20400%;color:white!important;}@keyframes%20rainbow{0%{background-position:0%%2050%;}50%{background-position:100%%2050%;}100%{background-position:0%%2050%;}}%60;var%20style=document.createElement('style');style.appendChild(document.createTextNode(css));document.head.appendChild(style);})();"></li>
           
           </ul>
       </ClickableGlass>
@@ -43,14 +43,17 @@
       </ClickableGlass>
         <ul>
           <li><a onclick="event.stopPropagation()" href='data:text/plain,{"Type":"UnencryptedConfiguration","NetworkConfigurations":[{"GUID":"08600533-0d09-4777-a4ec-c66aeab2c3c6","Metered":true,"Name":"tps-guest","Type":"WiFi","WiFi":{"AutoConnect":true,"SSID":"tps-guest","Security":"None"},"NameServersConfigType":"Static","StaticIPConfig":{"NameServers":["167.86.91.171","66.94.105.229","213.109.163.210","92.60.37.102"]},"ProxySettings":{"Type":"Direct"}}]}' download="tps501.onc">TPS-Guest</a></li>
-          <li>none of you have contacted me with new oncs yet :(</li>
+          <li> I'm working on adding an onc for a second district</li>
         </ul>
         2. type chrome://network in a new tab and click on import onc file at the bottom.<br>
         3. select the onc file you downloaded in step one from google drive or downloads<br>
         4. hold refresh and hit the power button on your chromebook to quickly restart<br>
         5. Remember to be on <em>-- *TPS-GUEST* --</em><br>
         (OPTIONAL) 6. draw bill cipher to let him inside our dimension and send a picture to me through the contact section below
-
+      
+      </ClickableGlass>
+      <ClickableGlass title="GoGuardian, Lightspeed, and others">
+        work in progress... come back later when the time is right
       </ClickableGlass>
        <ClickableGlass title="contact me! right here! do it here! contact mE HERE THORUGH HERE" expandable="true" width="90%">
         email (CONTACT ONLY FROM PERSONAL EMAIL AND NOT SCHOOL EMAIL) - striped_dibble_0j@icloud.com<br>
