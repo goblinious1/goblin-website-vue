12b68e75acd80cd99412d931e5ab02d753ee5108
goblinious1 <112513403+goblinious1@users.noreply.github.com>
2025-01-29 12:33:14 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,13 @@
   <div class="content" width="100%">
      
     <GlassContainer id="dropdowns">
-      <ClickableGlass title="DragMark (USE TO UNBLOCK THIS WEBSITE)" expandable="true" width="90%">
+      <ClickableGlass title="DragMark (use to unblock this website on your computer :3)" expandable="true" width="90%">
         1. create a bookmark to a link that's blocked (like <a href="securly.com.webcr.top">securly.com.webcr.top</a>) by just editing any bookmark and changing the URL to something<br>
         2. on a non-blocked page, drag the bookmark into the center of the screen<br>
-        3. PROFIT!!!
+        3. PROFIT!!!<br>
+        If you arent using securly and this works for you contact me using the form/email below so i can label this accordingly
       </ClickableGlass>
-      <ClickableGlass title="Cookie Corruption (main unblocking exploit)" expandable="true" width="90%">
+      <ClickableGlass title="Cookie Corruption (securly)" expandable="true" width="90%">
         1. create a bookmark for any page, and right click on it and select edit.<br>
         2. paste the text below into the "url" box<br>
         javascript:document.cookie='crextn_clear_cache_at=2024=01-21T05:59:59+00:00;'<br>
@@ -20,6 +21,13 @@
       </ClickableGlass>
      
      
+      
+      <ClickableGlass title="omadadns (all filter extensions)" expandable="true" width="90%">
+        https://docs.titaniumnetwork.org/kajigs/omadadns/
+      </ClickableGlass>
+      <ClickableGlass title="GoGuardian, Lightspeed, and others" expandable="true" width="90%">
+        try dragtab, if it works let me know at the contact section
+      </ClickableGlass>
       <ClickableGlass title="Funny little js thingies!" expandable="true" width="90%">
         to use these you gotta make a bookmark, edit it and paste the thing below as the url, then just click the bookmark to activate the thingy<br>
         <ul>
@@ -31,29 +39,6 @@
           <li><input type="text" readonly value="javascript:(function(){var speed=prompt(&quot;Enter the animation speed in seconds (default is 5):&quot;,&quot;5&quot;);if(speed===null||speed.trim()===&quot;&quot;||isNaN(speed)){speed=5;}var css=`*:not(img):not(svg){background-image:linear-gradient(270deg,#FF0000,#FF7F00,#FFFF00,#00FF00,#0000FF,#4B0082,#8B00FF);animation:rainbow%20${speed}s%20infinite;background-size:400%%20400%;color:white!important;}@keyframes%20rainbow{0%{background-position:0%%2050%;}50%{background-position:100%%2050%;}100%{background-position:0%%2050%;}}%60;var%20style=document.createElement('style');style.appendChild(document.createTextNode(css));document.head.appendChild(style);})();"></li>
           
           </ul>
-      </ClickableGlass>
-      <ClickableGlass title="onc exploit (LEGACY)" expandable="true" width="90%">
-        THIS EXPLOIT IS DEPRECATED. ONCS NO LONGER SEEM TO WORK FOR CONFIGURING NETWORKS :( You can still set your DNS to the nameservers for omadadns manually tho!
-        1. Download the ONC file coresponding to your district's wifi <i>"but my district isnt here?"</i> refer to "add your district" under this section<br>
-        <ClickableGlass title="add my district's onc" expandable="true" width="90%">
-        1. go to chrome://network<br>
-        2. click network state at the top<br>
-        3. click the plus next to the districts wifi<br>
-        4. copy all the text inside and send it to me at striped_dibble_0j@icloud.com WITH A **<b>PERSONAL</b>** EMAIL ADDRESS, NOT A SCHOOL EMAIL ADDRESS
-      </ClickableGlass>
-        <ul>
-          <li><a onclick="event.stopPropagation()" href='data:text/plain,{"Type":"UnencryptedConfiguration","NetworkConfigurations":[{"GUID":"08600533-0d09-4777-a4ec-c66aeab2c3c6","Metered":true,"Name":"tps-guest","Type":"WiFi","WiFi":{"AutoConnect":true,"SSID":"tps-guest","Security":"None"},"NameServersConfigType":"Static","StaticIPConfig":{"NameServers":["167.86.91.171","66.94.105.229","213.109.163.210","92.60.37.102"]},"ProxySettings":{"Type":"Direct"}}]}' download="tps501.onc">TPS-Guest</a></li>
-          <li> I'm working on adding an onc for a second district</li>
-        </ul>
-        2. type chrome://network in a new tab and click on import onc file at the bottom.<br>
-        3. select the onc file you downloaded in step one from google drive or downloads<br>
-        4. hold refresh and hit the power button on your chromebook to quickly restart<br>
-        5. Remember to be on <em>-- *TPS-GUEST* --</em><br>
-        (OPTIONAL) 6. draw bill cipher to let him inside our dimension and send a picture to me through the contact section below
-      
-      </ClickableGlass>
-      <ClickableGlass title="GoGuardian, Lightspeed, and others" expandable="true" width="90%">
-        work in progress... come back later when the time is right, in the meantime try dragtab, if it works, email me and let me know!
       </ClickableGlass>
        <ClickableGlass title="contact me! right here! do it here! contact mE HERE THORUGH HERE" expandable="true" width="90%">
         email (CONTACT ONLY FROM PERSONAL EMAIL AND NOT SCHOOL EMAIL) - striped_dibble_0j@icloud.com<br>
