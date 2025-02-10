b076f5b99cbdfa6be73dfcaeb163f2c9ba53a058
goblinious1 <112513403+goblinious1@users.noreply.github.com>
2025-02-10 10:45:02 -0600

    add clarity tracking tag

--- src/App.vue
+++ src/App.vue
@@ -65,6 +65,11 @@ hi this is a message !!!!!
 </template>
 
 <script>
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "q7qjmmclvo");
 
 import ClickableGlass from './components/ClickableGlass.vue';
 import GlassContainer from './components/GlassContainer.vue'
